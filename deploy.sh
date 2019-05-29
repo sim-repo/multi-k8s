994f572dd168770638270eff0ba5c7628148d36a
Igor <ivanov.igor.it@gmail.com>
2019-05-29 20:02:37 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t neogeowild/multi-client:latest -t neogeowild/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t neogeowild/multi-server:latest -t neogeowild/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t neogeowild/multi-worker:latest -t neogeowild/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push neogeowild/multi-client:latest
+docker push neogeowild/multi-server:latest
+docker push neogeowild/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push neogeowild/multi-client:$SHA
+docker push neogeowild/multi-server:$SHA
+docker push neogeowild/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=neogeowild/multi-server:$SHA
+kubectl set image deployments/client-deployment client=neogeowild/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=neogeowild/multi-worker:$SHA
